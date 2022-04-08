d89b6e083a4f695075fa4bfffa9bce9646293fc6
Matt Turner <user.personal@users.noreply.github.com>
2022-04-08 16:35:02 +0100

    Attempts to fix gmmk/pro

--- keyboards/gmmk/pro/iso/keymaps/mt-inside/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/mt-inside/keymap.c
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12      Prt           Rotary(Mute)
+//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12      Prt               Rotary(Mute)
 //      ~        1        2        3        4        5        6        7        8        9        0         -       (=)      BackSpc           Del
 //      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]                          PgUp
 //      Caps     A        S        D        F        G        H        J        K        L        ;        "        #        Enter             PgDn
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
     [0] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_KP_ENTER,      KC_MUTE,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
@@ -55,33 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, RGB_TOG, _______, _______, _______, RESET,   NK_TOGG, _______, _______, _______, _______,          _______, KC_KP_8, _______,
         _______,          _______, _______,                   KC_MUTE,                            _______, _______, _______, KC_KP_4, KC_KP_2, KC_KP_6
     ),
-
-    [2] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-        _______,          _______, _______,                   _______,                            _______, _______, _______, _______, _______, _______
-    ),
-
-    [3] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-        _______,          _______, _______,                   _______,                            _______, _______, _______, _______, _______, _______
-    ),
 };
 // clang-format on
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (clockwise) {
-        tap_code(KC_VOLU);
+        tap_code(KC_F12);
     } else {
-        tap_code(KC_VOLD);
+        tap_code(KC_F11);
     }
     return true;
 }
--- keyboards/gmmk/pro/iso/keymaps/mt-inside/rules.mk
+++ keyboards/gmmk/pro/iso/keymaps/mt-inside/rules.mk
@@ -1,2 +1,2 @@
-VIA_ENABLE = yes
+VIA_ENABLE = no # This brakes part of the keymap (maybe those keys are off the end of the EEPROM?), eg can't control the codes from the rotary encoder
 # RAW_ENABLE = yes - no RawHID yet for this board?
